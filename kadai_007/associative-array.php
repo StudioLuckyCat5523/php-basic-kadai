40b88fc2e073e108416535e5855e1230f5be59f6
StudioLuckyCat5523 <162400926+StudioLuckyCat5523@users.noreply.github.com>
2025-02-12 10:05:31 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+ <meta charset="UTF-8">
+ <title>kadai_007</title>
+</head>
+
+<body>
+ <p>
+   <?PHP
+    //連想配列に値を導入する
+    $onion_date = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    //連想配列の値を出力する
+    print_r($onion_date);
+
+    
+    ?>
+</p>
+</body>
+</html>
